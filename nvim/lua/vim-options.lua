091a93d7e85fc46b0f3cdd9c5883044ab9bd6fd9
Dmitry Artiushenko <dmitry.artiushenko@gmail.com>
2024-10-22 21:03:57 +0200

    first config

---
+++ nvim/lua/vim-options.lua
@@ -0,0 +1,10 @@
+vim.g.mapleader = " "
+
+vim.keymap.set('n', '<C-s>', ':w<CR>', {})
+
+vim.cmd("set number")
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+
